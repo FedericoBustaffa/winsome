4a30e92491866ad2b95a5841a286653c937d0b11
FedericoBustaffa <kiko.bustaffa@gmail.com>
2022-06-14 18:05:40 +0200

    user

--- src/Winsome.java
+++ src/Winsome.java
@@ -1,5 +1,6 @@
+
 public class Winsome {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
     }
 }
